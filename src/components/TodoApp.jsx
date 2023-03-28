51e05470b7a77c0511f2c591dd4f7b1861377168
Daniel <dselasea@gmail.com>
2023-03-28 04:09:40 +0000

    Add dropdown and modal component. Add navbar component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../Navbar'
 import Header from './Header'
 import TodosLogic from './TodosLogic'
 
 const TodoApp = () => {
   return (
     <React.Fragment>
+      <Navbar />
       <Header />
       <TodosLogic />
     </React.Fragment>
