0190550b06885f23ddcf0bfb394eb07cd134b776
Daniel <dselasea@gmail.com>
2023-04-02 03:05:55 +0000

    Add input field for editing;

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
 const TodoItem = ({itemProp, handleChange, delTodo}) => {
+  const [editing, setEditing] = useState(false);
   const completedStyle = {
     fontStyle: 'italic',
     color: '#595959',
     opacity: 0.4,
     textDecoration: 'line-through',
   };
+  const handleEditing = () => {
+    setEditing(true);
+  }
   return (
     <li className={styles.item}>
       <div className={styles.content}>
@@ -15,9 +19,11 @@ const TodoItem = ({itemProp, handleChange, delTodo}) => {
         type="checkbox" 
         checked={itemProp.completed}
         onChange={() => handleChange(itemProp.id)} />
+        <button onClick={handleEditing}>Edit</button>
         <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       <span style={itemProp.completed ? completedStyle : null}>{itemProp.title}</span>
       </div>
+      <input type="text" value={itemProp.title} className={styles.textInput} />
     </li>
   )
 }
