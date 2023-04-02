fd93337374fc7fdc90beea4850e69ef6ec05afe4
Daniel <dselasea@gmail.com>
2023-04-02 03:23:19 +0000

    Add editing functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
-const TodoItem = ({itemProp, handleChange, delTodo}) => {
+const TodoItem = ({itemProp, handleChange, delTodo, setUpdate}) => {
   const [editing, setEditing] = useState(false);
   const completedStyle = {
     fontStyle: 'italic',
@@ -9,12 +9,26 @@ const TodoItem = ({itemProp, handleChange, delTodo}) => {
     opacity: 0.4,
     textDecoration: 'line-through',
   };
+
+  let viewMode = {};
+  let editMode = {};
+  if(editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
   const handleEditing = () => {
     setEditing(true);
   }
+
+  const handleUpdateDone = (event) => {
+    if(event.key === 'Enter') {
+      setEditing(false);
+    }
+  }
   return (
     <li className={styles.item}>
-      <div className={styles.content}>
+      <div className={styles.content} style={viewMode}>
       <input 
         type="checkbox" 
         checked={itemProp.completed}
@@ -23,7 +37,7 @@ const TodoItem = ({itemProp, handleChange, delTodo}) => {
         <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       <span style={itemProp.completed ? completedStyle : null}>{itemProp.title}</span>
       </div>
-      <input type="text" value={itemProp.title} className={styles.textInput} />
+      <input type="text" onChange={(e) => setUpdate(e.target.value, itemProp.id)} onKeyDown={handleUpdateDone} value={itemProp.title} className={styles.textInput} style={editMode} />
     </li>
   )
 }
